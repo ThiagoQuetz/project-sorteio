681169bcee1cfd672420b31f908843f9525afaaa
ThiagoQuetz <lizplanejadosjf@gmail.com>
2024-09-06 21:52:20 -0300

    fix-code-scripts

--- scripts.js
+++ scripts.js
@@ -9,7 +9,7 @@ function generateNumber() {
 
         const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-        alert("Seu resultado é:" + result)
+        alert("Seu resultado é: " + result)
     }
 
 }
